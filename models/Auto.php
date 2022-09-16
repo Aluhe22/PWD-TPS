c2216d4bfef627fc771a2aa784a586e503dca31c
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2022-09-16 13:30:28 -0300

    Cambios en Auto.php

--- models/Auto.php
+++ models/Auto.php
@@ -1,5 +1,6 @@
 <?php
-class Auto{
+class Auto
+{
     private $patente;
     private $marca;
     private $modelo;
@@ -17,62 +18,99 @@ class Auto{
 
     public function cargar($patente, $marca, $modelo, $dniDuenio, $mensaje)
     {
-        $this->patente = $patente;
-        $this->marca = $marca;
-        $this->modelo  = $modelo;
-        $this->rDniDuenio = $dniDuenio;
-        $this->mensaje = $mensaje;
+        $this->setPatente($patente);
+        $this->setMarca($marca);
+        $this->setModelo($modelo);
+        $this->setRDniDuenio($dniDuenio);
+        $this->setMensaje($mensaje);
     }
 
     //Metodos de acceso
-    public function getModelo(){
+    public function getModelo()
+    {
         return $this->modelo;
     }
 
-    public function setModelo($modelo){
+    public function setModelo($modelo)
+    {
         $this->modelo = $modelo;
     }
 
-    public function getMarca(){
+    public function getMarca()
+    {
         return $this->marca;
     }
 
-    public function setMarca($marca){
+    public function setMarca($marca)
+    {
         $this->marca = $marca;
     }
 
-    public function getPatente(){
+    public function getPatente()
+    {
         return $this->patente;
     }
 
-    public function setPatente($patente){
+    public function setPatente($patente)
+    {
         $this->patente = $patente;
     }
 
-    public function getRDniDuenio(){
+    public function getRDniDuenio()
+    {
         return $this->rDniDuenio;
     }
 
-    public function setRDniDuenio($rDniDuenio){
+    public function setRDniDuenio($rDniDuenio)
+    {
         $this->rDniDuenio = $rDniDuenio;
     }
 
-    public function getMensaje(){
+    public function getMensaje()
+    {
         return $this->mensaje;
     }
 
-    public function setMensaje($mensaje){
+    public function setMensaje($mensaje)
+    {
         $this->mensaje = $mensaje;
     }
 
     public function __toString()
     {
-        return "Patente: " . $this->getPatente() . 
-        "\nMarca: " . $this->getMarca() . 
-        "\nModelo: " . $this->getModelo() . 
-        "\nDueño: " . $this->getRDniDuenio();
+        return "Patente: " . $this->getPatente() .
+            "\nMarca: " . $this->getMarca() .
+            "\nModelo: " . $this->getModelo() .
+            "\nDueño: " . $this->getRDniDuenio();
     }
 
     //Funciones BD
-    
+
+    //BUSCAR
+    public function buscar($patente)
+    {
+        $base = new BaseDatos();
+        $resp = false;
+        $sql = "SELECT * FROM Auto WHERE Patente = " . $patente;
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($sql)) {
+                if($row2 = $base->Registro()){
+                    $this->setPatente($row2['Patente']);
+                    $this->setMarca($row2['Marca']);
+                    $this->setModelo($row2['Modelo']);
+                    //Creo un objeto persona para buscar al duenio y setear el objeto
+                    $duenio = new Persona();
+                    $duenio->buscar($row2['DniDuenio']);
+                    $this->setRDniDuenio($duenio);
+                    $resp = true;
+                }
+            } else {
+                $this->setMensaje($base->getError());
+            }
+        } else {
+            $this->setMensaje($base->getError());
+        }
+    }
+
+    //LISTAR
 }
